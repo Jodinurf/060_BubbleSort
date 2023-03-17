c5f31a498c1a56ad69090d81d09e81faed0d2078
Jodinurf <jodinurfarkhani04@gmail.com>
2023-03-17 16:03:35 +0700

    menambahkan prosedur untuk input data pada array dan menambahkan komentar

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -1,9 +1,19 @@
-
 #include <iostream>
 using namespace std;
 
-int a[20];
-int n;
+int a[20];		//Deklarasi array a dengan ukuran 20
+int n;			//Deklarasi variabel n untuk menyimpan banyak elemen pada array
+
+void input() {	//Procedur untuk input
+	while (true) {	//Looping
+		cout << "Masukkan banyaknya elemen pada array : ";	//Output ke Layar
+		cin >> n;	//input dari pengguna 
+		if (n <= 20)	//Jika n kurang dari atau sama dengan 20
+			break;	//Keluar dari loop
+		else {	//Jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";	//output ke Layar
+		}
+	}
 
 
 
